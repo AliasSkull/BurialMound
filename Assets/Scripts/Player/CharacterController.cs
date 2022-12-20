340f6c57a5c1313a806e7d4bca093e0d615db2f2
Kate Jarvis <katejarvis1@hotmail.com>
2022-12-20 12:04:34 -0500

    Update CharacterController.cs
    Player now moves towards mouse click

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -8,9 +8,9 @@ public class CharacterController : MonoBehaviour
 {
    
     private float moveSpeed = 7f;
-    private float stoppingDistance = 1f;
+    private float stoppingDistance = 2f;
 
-    Vector3 forward, right;
+ 
 
     Animator animator;
 
@@ -20,59 +20,43 @@ public class CharacterController : MonoBehaviour
 
     private Vector3 targetPosition;
 
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         _currentState = State.Combat;
-       /* forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;*/
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-           
-         
 
-                if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
-                {
-                    Vector3 moveDirection = (targetPosition - transform.position).normalized;
-                    transform.position += moveDirection * Time.deltaTime * moveSpeed;
-
-                }
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
 
 
-                if (Input.GetKeyDown(KeyCode.T))
-                {
-                    CombatMove( new Vector3(54, 0, 35));
-                
-                }
+        if (Input.GetMouseButtonDown(0))
+        { 
 
-         
- 
-        
+            targetPosition = MousetoWorld.GetPosition();
+            Move(targetPosition);
+        }
     }
 
-    public void Move() 
-    {
 
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal Key"), 0, Input.GetAxis("Vertical Key"));
-        Vector3 rightmovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal Key");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical Key");
-
-        Vector3 heading = Vector3.Normalize(rightmovement + upMovement);
-
-        transform.forward = heading;
-        transform.position += rightmovement;
-        transform.position += upMovement;
-
-    }
-
-    private void CombatMove(Vector3 targetPosition) 
+    private void Move(Vector3 targetPosition) 
     {
 
        
